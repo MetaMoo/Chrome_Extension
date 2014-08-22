8902d5781d32c3e7435f338c8874fa21132dfc76
ddrieck <ddrieck@gmail.com>
2014-08-21 22:33:32 -0700

    Finished styling. Need to figure out 503 error and click not closing modal.

--- content.js
+++ content.js
@@ -9,14 +9,17 @@ selectedText = '';
 modalForm();
 
 function modalForm(){
-	$.modal("<img class='logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><form method='post' action='' name='metamoo-form' id='metamoo-form'><div class='form-group'><label class='form-label tag-label' for='tag'>Tag</label><input class='form-control tag' name='tag' type='text'></input></div><div class='form-group'><label class='form-label note-label' for='note'>Note</label><textarea class='form-control note' name='note' type='text'></textarea></div><button class='form-btn' value='submit' type='submit'>Submit</button><button class='form-btn cancel-btn' value='cancel'>Cancel</button></div></form>", {
+	$.modal("<img class='logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><form method='post' action='' name='metamoo-form' id='metamoo-form'><div class='form-group'><label class='form-label tag-label' for='tag'>Tag</label><input class='form-control tag' name='tag' type='text'></input></div><div class='form-group form-group-note'><label class='form-label note-label' for='note'>Note</label><textarea class='form-control note' name='note' type='text'></textarea></div><div class='buttons'><button class='form-btn' value='submit' type='submit'>Submit</button><button class='form-btn cancel-btn' value='cancel'>Cancel</button></div></form><p class='warning-text'>Please make sure you are logged into <a href='http://metamoowebapp.herokuapp.com/login' target='_blank'>MetaMoo</a></p>", {
 	containerCss:{
 		backgroundColor:"#fff", 
-		borderColor:"#676767", 
-		height:"40%", 
-		padding:"2%", 
+		borderColor:"#676767",
+		borderWidth: "1px",
+		borderStyle: "solid", 
+		height:"43%", 
+		padding:"1%", 
 		width:"40%",
-		right: "50px"
+		left: "60%",
+		top: "50px"
 	}}, {onOpen: function (dialog) {
 	dialog.overlay.fadeIn('slow', function () {
 		dialog.container.slideDown('slow', function () {
@@ -27,13 +30,17 @@ function modalForm(){
 };
 
 function modalLoginPrompt(){
-	$.modal("<div><h1>MetaMoo</h1></div><h1>Please visit MetaMoo to Login</h1><p>Logged in? <a href='#'>Continue</a></div>", {
+	$.modal("<div class='login-warning'><img class='warning-logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><h3 class='warning-paragraph'>Unfortunately, we can't submit your data unless you are logged into MetaMoo.</h3><p>Log in <a class='login-link' href='http://metamoowebapp.herokuapp.com/login' target='_blank'>here</a>.</div>", {
 	containerCss:{
 		backgroundColor:"#fff", 
-		borderColor:"#fff", 
-		height:"30%", 
-		padding:0, 
-		width:"40%"
+		borderColor:"#676767",
+		borderWidth: "1px",
+		borderStyle: "solid", 
+		height:"43%", 
+		padding:"1%", 
+		width:"40%",
+		left: "60%",
+		top: "50px"
 	}}, {onOpen: function (dialog) {
 	dialog.overlay.fadeIn('slow', function () {
 		dialog.container.slideDown('slow', function () {
@@ -54,6 +61,11 @@ $('.cancel-btn').click(function(){
 	event.preventDefault();
 	$.modal.close();
 	console.log("cancelling")
+});
+
+$('#metamoo-form').on('click', '.login-link', function(){
+	$.modal.close();
+	console.log("click the link")
 });
 
 $("#metamoo-form").submit(function(event){
