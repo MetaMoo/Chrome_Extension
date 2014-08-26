4ae6da14ef080ad7cf39be217b0f311ccf7d663a
ddrieck <ddrieck@gmail.com>
2014-08-26 11:58:50 -0700

    Updated styling with ids to sandbox form styles. Remove extraneous files.

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ selectedText = '';
 modalForm();
 
 function modalForm(){
-	$.modal("<img class='logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><form method='post' action='' name='metamoo-form' id='metamoo-form'><div class='form-group'><label class='form-label tag-label' for='tag'>Tag</label><input class='form-control tag' name='tag' type='text'></input></div><div class='form-group form-group-note'><label class='form-label note-label' for='note'>Note</label><textarea class='form-control note' name='note' type='text'></textarea></div><div class='buttons'><button class='form-btn' value='submit' type='submit'>Submit</button><button class='form-btn cancel-btn' value='cancel'>Cancel</button></div></form><p class='warning-text'>Please make sure you are logged into <a href='http://metamoowebapp.herokuapp.com/login' target='_blank'>MetaMoo</a></p>", {
+	$.modal("<a id='metamoo-link' href='http://metamoowebapp.herokuapp.com'><img id='metamoo-logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'></a><form method='post' action='' name='metamoo-form' id='metamoo-form'><div class='metamoo-form-group'><label id='metamoo-form-label' for='tag'>Tag</label><input id='metamoo-form-control' name='tag' type='text'></input></div><div id='metamoo-form-group-note'><label id='metamoo-form-label' for='note'>Note</label><textarea id='metamoo-note' name='note' type='text'></textarea></div><div id='metamoo-buttons'><button id='metamoo-form-btn' value='submit' type='submit'>Submit</button><button id='metamoo-form-btn' value='cancel'>Cancel</button></div></form><p id='metamoo-warning-text'>Please make sure you are logged into <a id='metamoo-link' href='http://metamoowebapp.herokuapp.com/login' target='_blank'>MetaMoo</a></p><p id='metamoo-warning-text'>Hit ESC to close the window.</p>", {
 	containerCss:{
 		backgroundColor:"#fff", 
 		borderColor:"#676767",
@@ -30,7 +30,7 @@ function modalForm(){
 };
 
 function modalLoginPrompt(){
-	$.modal("<div class='login-warning'><img class='warning-logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><h3 class='warning-paragraph'>Unfortunately, we can't submit your data unless you are logged into MetaMoo.</h3><p>Log in <a class='login-link' href='http://metamoowebapp.herokuapp.com/login' target='_blank'>here</a>.</div>", {
+	$.modal("<div id='metamoo-login-warning'><img id='meatmoo-warning-logo' src='http://metamoowebapp.herokuapp.com/images/main_logo.png'><h3 id='metamoo-warning-paragraph'>Unfortunately, we can't submit your data unless you are logged into MetaMoo.</h3><p id='metamoo-login-text'>Log in <a id='metamoo-login-link' href='http://metamoowebapp.herokuapp.com/login' target='_blank'>here</a>.</div>", {
 	containerCss:{
 		backgroundColor:"#fff", 
 		borderColor:"#676767",
